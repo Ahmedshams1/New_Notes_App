e0e36a586a6d293c703d80f10b53a2deb1c41977
Ahmedshams1 <ahmedsama111997@gmail.com>
2024-03-31 12:05:33 +0200

    color changes

--- lib/widgets/colorsListView.dart
+++ lib/widgets/colorsListView.dart
@@ -1,29 +1,65 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
-
+  const ColorItem({super.key, required this.isActive, required this.color});
+  final bool isActive;
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return CircleAvatar(
-      radius: 38,
-      backgroundColor: Colors.green,
-    );
+    return isActive
+        ? CircleAvatar(
+            radius: 38,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              radius: 36,
+              backgroundColor: color,
+            ),
+          )
+        : CircleAvatar(
+            radius: 38,
+            backgroundColor: color,
+          );
   }
 }
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
 
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> color = [
+    Colors.black,
+    Colors.white,
+    Colors.red,
+    Colors.deepOrange,
+    Colors.pink,
+    Colors.yellow
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 38 * 2,
       child: ListView.builder(
-          itemCount: 20,
+          itemCount: color.length,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
-            return ColorItem();
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: GestureDetector(
+                onTap: () {
+                  currentIndex = index;
+                  setState(() {});
+                },
+                child: ColorItem(
+                  color: color[index],
+                  isActive: currentIndex == index,
+                ),
+              ),
+            );
           }),
     );
   }
